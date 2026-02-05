f87f097873b5193e1fa1233133c2e28313078b7b
sh2687 <sh2687@cornell.edu>
2026-02-04 23:09:10 -0500

    Meow

--- lab1/DisplayUnopt_GL.v
+++ lab1/DisplayUnopt_GL.v
@@ -16,14 +16,48 @@ module DisplayUnopt_GL
   output wire [6:0] seg_ones
 );
 
-  //''' LAB ASSIGNMENT '''''''''''''''''''''''''''''''''''''''''''''''''''
-  // Implement the unoptimized five-bit numeric display
-  //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+//========================================================================
+// DisplayUnopt_GL
+//========================================================================
 
-  // remove these lines before starting your implementation
-  `ECE2300_UNUSED( in );
-  `ECE2300_UNDRIVEN( seg_tens );
-  `ECE2300_UNDRIVEN( seg_ones );
+`ifndef DISPLAY_UNOPT_GL_V
+`define DISPLAY_UNOPT_GL_V
+
+`include "lab1/BinaryToBinCodedDec_GL.v"
+`include "lab1/BinaryToSevenSegUnopt_GL.v"
+
+module DisplayUnopt_GL
+(
+  input  wire [4:0] in,
+  output wire [6:0] seg_tens,
+  output wire [6:0] seg_ones
+);
+
+  wire [3:0] tens;
+  wire [3:0] ones;
+
+  BinaryToBinCodedDec_GL bcd
+  (
+    .in   ( in   ),
+    .tens ( tens ),
+    .ones ( ones )
+  );
+
+BinaryToSevenSegUnopt_GL tens_display
+(
+    .in  ( tens     ),
+    .seg ( seg_tens )
+);
+
+BinaryToSevenSegUnopt_GL ones_display
+(
+    .in  ( ones     ),
+    .seg ( seg_ones )
+);
+
+endmodule
+
+`endif /* DISPLAY_UNOPT_GL_V */
 
 endmodule
 
