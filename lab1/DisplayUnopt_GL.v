f8a20ec70ac149a193f87ccc0b7fce54208bc779
sh2687 <sh2687@cornell.edu>
2026-02-04 23:11:00 -0500

    Meow

--- lab1/DisplayUnopt_GL.v
+++ lab1/DisplayUnopt_GL.v
@@ -16,50 +16,28 @@ module DisplayUnopt_GL
   output wire [6:0] seg_ones
 );
 
-//========================================================================
-// DisplayUnopt_GL
-//========================================================================
+wire [3:0] tens;
+wire [3:0] ones;
 
-`ifndef DISPLAY_UNOPT_GL_V
-`define DISPLAY_UNOPT_GL_V
-
-`include "lab1/BinaryToBinCodedDec_GL.v"
-`include "lab1/BinaryToSevenSegUnopt_GL.v"
-
-module DisplayUnopt_GL
+BinaryToBinCodedDec_GL bcd
 (
-  input  wire [4:0] in,
-  output wire [6:0] seg_tens,
-  output wire [6:0] seg_ones
+  .in   ( in   ),
+  .tens ( tens ),
+  .ones ( ones )
 );
-
-  wire [3:0] tens;
-  wire [3:0] ones;
-
-  BinaryToBinCodedDec_GL bcd
-  (
-    .in   ( in   ),
-    .tens ( tens ),
-    .ones ( ones )
-  );
 
 BinaryToSevenSegUnopt_GL tens_display
 (
-    .in  ( tens     ),
-    .seg ( seg_tens )
+  .in  ( tens     ),
+  .seg ( seg_tens )
 );
 
 BinaryToSevenSegUnopt_GL ones_display
 (
-    .in  ( ones     ),
-    .seg ( seg_ones )
+  .in  ( ones     ),
+  .seg ( seg_ones )
 );
 
 endmodule
-
-`endif /* DISPLAY_UNOPT_GL_V */
-
-endmodule
-
-`endif /* DISPLAY_UNOPT_GL_V */
-
+`endif 
+/* DISPLAY_UNOPT_GL_V */
